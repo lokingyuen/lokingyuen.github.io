440a0833e0d4b8d3f2c90c6dcef03fc54951f71a
King LO <lokingyuen777@gmail.com>
2022-10-12 23:08:19 +0800

    Site updated: 2022-10-12 23:08:18

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  autoplay: false,
+  audio: [
+    {
+      name: "In The Shadow Of The Sun",
+      artist: 'Professor Green',
+      url: 'https://drive.google.com/uc?export=download&id=1cVL2n7_hlFj2jaNG_P93TUXNP-FSFgdd',
+      cover: 'https://s2.loli.net/2022/10/12/LJ4OkTFtSK2GCcs.jpg',
+    },
+  ]
+});
